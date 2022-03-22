c00e2395f2ec83ec6bec096315c80c091139882b
LAPTOP-T1J5NJSM\Ejer <filipjessen@gmail.com>
2022-03-22 10:39:46 +0100

    overview file made

---
+++ TodoList/Pages/Todo.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace TodoList.Pages
+{
+    partial class Todo
+    {
+        public List<TodoItem> todos = new();
+        public string? newTodo { get; set; }
+        
+        public string? Titel { get; set; }
+
+        private void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            Titel = "EUX H5 Todo list";
+            base.OnInitialized();
+        }
+    }
+}
